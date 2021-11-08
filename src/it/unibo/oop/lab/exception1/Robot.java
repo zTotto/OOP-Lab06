f2d9e24e5b0df8b3266146fe09310daf54ab06df
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2021-11-08 16:45:43 +0100

    Made Robot compatible with the changes made to RobotEnvironment

--- src/it/unibo/oop/lab/exception1/Robot.java
+++ src/it/unibo/oop/lab/exception1/Robot.java
@@ -83,17 +83,11 @@ public class Robot {
      *            the new Y position to move the robot to
      * @return true if robot gets moved, false otherwise
      */
-    private boolean moveToPosition(final int newX, final int newY) {
+    private boolean moveToPosition(final int newX, final int newY) throws PositionOutOfBoundException{
         boolean returnValue = true;
         if (this.isBatteryEnoughToMove()) {
-            if (this.environment.move(newX, newY)) {
                 this.consumeBatteryForMovement();
                 this.log("Moved to position(" + newX + "," + newY + ").");
-            } else {
-                this.log("Can not move to (" + newX + "," + newY
-                        + ") the robot is touching at least one world boundary");
-                returnValue = false;
-            }
         } else {
             this.log("Can not move to position(" + newX + "," + newY + "). Not enough battery.");
             returnValue = false;
