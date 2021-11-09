edfcac39113b96273c5af0b3c26db74a0f323e71
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2021-11-09 11:09:17 +0100

    Added exercise 4

--- src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
@@ -1,7 +1,10 @@
 package it.unibo.oop.lab.collections2;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -13,70 +16,82 @@ import java.util.List;
  * 1) complete the definition of the methods by following the suggestions
  * included in the comments below.
  * 
- * @param <U>
- *            Specific user type
+ * @param <U> Specific user type
  */
 public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
 
-    /*
-     * 
-     * [FIELDS]
-     * 
-     * Define any necessary field
-     * 
-     * In order to save the people followed by a user organized in groups, adopt
-     * a generic-type Map:
-     * 
-     * think of what type of keys and values would best suit the requirements
-     */
+	/*
+	 * 
+	 * [FIELDS]
+	 * 
+	 * Define any necessary field
+	 * 
+	 * In order to save the people followed by a user organized in groups, adopt a
+	 * generic-type Map:
+	 * 
+	 * think of what type of keys and values would best suit the requirements
+	 */
 
-    /*
-     * [CONSTRUCTORS]
-     * 
-     * 1) Complete the definition of the constructor below, for building a user
-     * participating in a social network, with 4 parameters, initializing:
-     * 
-     * - firstName - lastName - username - age and every other necessary field
-     * 
-     * 2) Define a further constructor where age is defaulted to -1
-     */
+	private Map<U, String> followedUsers = new HashMap<>();
 
-    /**
-     * Builds a new {@link SocialNetworkUserImpl}.
-     * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param userAge
-     *            user's age
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
-     */
-    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
-    }
+	/*
+	 * [CONSTRUCTORS]
+	 * 
+	 * 1) Complete the definition of the constructor below, for building a user
+	 * participating in a social network, with 4 parameters, initializing:
+	 * 
+	 * - firstName - lastName - username - age and every other necessary field
+	 * 
+	 * 2) Define a further constructor where age is defaulted to -1
+	 */
 
-    /*
-     * [METHODS]
-     * 
-     * Implements the methods below
-     */
+	/**
+	 * Builds a new {@link SocialNetworkUserImpl}.
+	 * 
+	 * @param name    the user firstname
+	 * @param surname the user lastname
+	 * @param userAge user's age
+	 * @param user    alias of the user, i.e. the way a user is identified on an
+	 *                application
+	 */
+	public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+		super(name, surname, user, userAge);
+	}
 
-    @Override
-    public boolean addFollowedUser(final String circle, final U user) {
-        return false;
-    }
+	public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+		super(name, surname, user);
+	}
 
-    @Override
-    public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
-    }
+	/*
+	 * [METHODS]
+	 * 
+	 * Implements the methods below
+	 */
 
-    @Override
-    public List<U> getFollowedUsers() {
-        return null;
-    }
+	@Override
+	public boolean addFollowedUser(final String circle, final U user) {
+		if (!followedUsers.containsKey(user)) {
+			followedUsers.put(user, circle);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public Collection<U> getFollowedUsersInGroup(final String groupName) {
+		Collection<U> group = new LinkedList<>();
+		for (U user : followedUsers.keySet()) {
+			if (followedUsers.get(user) == groupName) {
+				group.add(user);
+			}
+		}
+		return group;
+	}
+
+	@Override
+	public List<U> getFollowedUsers() {
+		return new LinkedList<U>(followedUsers.keySet());
+	}
 
 }
--- src/it/unibo/oop/lab/collections2/TestSocialNetworkUser.java
+++ src/it/unibo/oop/lab/collections2/TestSocialNetworkUser.java
@@ -43,10 +43,10 @@ public final class TestSocialNetworkUser {
          * 
          * * Adam Smith, asmith, (no age)
          */
-        final SocialNetworkUser<User> kbacon = null;
-        final SocialNetworkUser<User> dwashington = null;
-        final SocialNetworkUser<User> mgladwell = null;
-        final SocialNetworkUser<User> ntaleb = null;
+        final SocialNetworkUser<User> kbacon = new SocialNetworkUserImpl<>("Kevin", "Bacon", "kbacon", 56);
+        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington", 59);;
+        final SocialNetworkUser<User> mgladwell = new SocialNetworkUserImpl<>("Malcom", "Gladwell", "mgladwell", 51);;
+        final SocialNetworkUser<User> ntaleb = new SocialNetworkUserImpl<>("Nicholas", "Taleb", "ntaleb", 54);;
         final User asmith = new UserImpl("Adam", "Smith", "asmith");
         /*
          * Make people follow each other
